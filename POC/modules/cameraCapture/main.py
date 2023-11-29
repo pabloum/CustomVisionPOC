1b0791005e32f8c1585b3974fb773abdca4735c8
Pablo Uribe <pablo.um00@gmail.com>
2023-11-28 21:51:56 -0500

    Refactor number of retries and split in smaller methods

--- POC/modules/cameraCapture/main.py
+++ POC/modules/cameraCapture/main.py
@@ -15,6 +15,9 @@ SENT_IMAGES = 0
 # global client
 CLIENT = None
 
+# GLOBAL RETRUES
+retries = 10
+
 # Send a message to IoT Hub
 # Route output1 to $upstream in deployment.template.json
 def send_to_hub(strMessage):
@@ -30,15 +33,18 @@ def sendFrameForProcessing(imagePath, imageProcessingEndpoint):
 
     with open(imagePath, mode="rb") as test_image:
         try:
-            response = requests.post(imageProcessingEndpoint, headers = headers, data = test_image)
-            # printJsonResponse(imagePath, response)
-            printSimplePrediction(imagePath, response)
+            for retry in range(1,retries):
+                response = requests.post(imageProcessingEndpoint, headers = headers, data = test_image)
+                if response.status_code == 200:
+                    # printJsonResponse(imagePath, response)
+                    printSimplePrediction(imagePath, response)
+                    return json.dumps(response.json())
         except Exception as e:
             print(e)
             print("No response from classification service")
             return None
 
-    return json.dumps(response.json())
+    return None
 
 def printJsonResponse(imagePath, response):
     print("Response from classification service for image " + imagePath + ": (" + str(response.status_code) + ") " + json.dumps(response.json()) + "\n")
@@ -51,27 +57,30 @@ def printSimplePrediction(imagePath, response):
     streetProbability = predictions[0]
     carProbability = predictions[1]
 
-    certainty = 0
+    mostLikelyTag = getMostLikelyTag(streetProbability, carProbability)
+    certainty = max([streetProbability, carProbability])
+    guessCorrectness = getGuessCorrectness(imagePath, mostLikelyTag)
+
+    print("The code is " + str(f"{certainty*100:.2f}")  + "%% sure that "  + imagePath + " is a f*cking " + mostLikelyTag +  guessCorrectness)
+
+def getMostLikelyTag(streetProbability, carProbability):
     if streetProbability >= 0.9:
-        guess = "STREET"
-        certainty = streetProbability
+        return "STREET"
     elif carProbability >= 0.9:
-        guess = "CAR"
-        certainty = carProbability
+        return "CAR"
     else: 
-        guess = "VERY SADLY UNKNOWN"
+        return "VERY SADLY UNKNOWN"
 
-    isGuessRight = ""
+def getGuessCorrectness(imagePath, guess):
     if ("street" in imagePath.lower() and guess == "STREET") or ("car" in imagePath.lower() and guess == "CAR"):
-        isGuessRight = "THE GUESS IS FUCKING RIGHT MOTHAFOCKA"
+        return ""
     else:
-        isGuessRight = "WARNING !!!!1 WRONG GUESS"
-
-    print("The code is " + str(f"{certainty*100:.2f}")  + "%% sure that "  + imagePath + " is a f*cking " + guess + " ---------------->" + isGuessRight)
+        return "----------------> WARNING !!!!1 WRONG GUESS"
 
 def get_jpg_files(folder_name):
     folder_path = os.path.join(os.path.dirname(__file__), folder_name)
     jpg_files = [file for file in os.listdir(folder_path) if file.lower().endswith(".jpg")]
+    jpg_files.sort()
     return jpg_files
 
 def main(imagePath, imageProcessingEndpoint):
@@ -94,7 +103,7 @@ def main(imagePath, imageProcessingEndpoint):
                 classification = sendFrameForProcessing(image, imageProcessingEndpoint)
                 if classification:
                     send_to_hub(classification)
-                time.sleep(5)
+                time.sleep(1)
 
     except KeyboardInterrupt:
         print ( "IoT Edge module sample stopped" )
@@ -102,10 +111,8 @@ def main(imagePath, imageProcessingEndpoint):
 if __name__ == '__main__':
     try:
         # Retrieve the image location and image classifying server endpoint from container environment
-        # IMAGE_PATH = os.getenv('IMAGE_PATH', "")
-        # IMAGE_PROCESSING_ENDPOINT = os.getenv('IMAGE_PROCESSING_ENDPOINT', "")
-        IMAGE_PATH = "ImagesToClassify/"
-        IMAGE_PROCESSING_ENDPOINT = "http://samplemodule/image"
+        IMAGE_PATH = os.getenv('IMAGE_PATH', "")
+        IMAGE_PROCESSING_ENDPOINT = os.getenv('IMAGE_PROCESSING_ENDPOINT', "")
     except ValueError as error:
         print ( error )
         sys.exit(1)
@@ -113,4 +120,4 @@ if __name__ == '__main__':
     if ((IMAGE_PATH and IMAGE_PROCESSING_ENDPOINT) != ""):
         main(IMAGE_PATH, IMAGE_PROCESSING_ENDPOINT)
     else: 
-        print ( "PUMO Test Error: Image path or image-processing endpoint PUMO missing" )
+        print ( "Test Error: Image path or image-processing endpoint missing" )
