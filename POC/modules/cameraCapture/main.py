34331b3def0112d5ec303141d7b11b6194243e10
Pablo Uribe <pablo.um00@gmail.com>
2023-11-28 20:57:22 -0500

    Improve logs

--- POC/modules/cameraCapture/main.py
+++ POC/modules/cameraCapture/main.py
@@ -22,7 +22,6 @@ def send_to_hub(strMessage):
     CLIENT.send_message_to_output(message, "output1")
     global SENT_IMAGES
     SENT_IMAGES += 1
-    print( "Total images sent: {}".format(SENT_IMAGES) )
 
 # Send an image to the image classifying server
 # Return the JSON response from the server with the prediction result
@@ -32,13 +31,43 @@ def sendFrameForProcessing(imagePath, imageProcessingEndpoint):
     with open(imagePath, mode="rb") as test_image:
         try:
             response = requests.post(imageProcessingEndpoint, headers = headers, data = test_image)
-            print("Response from classification service for image " + imagePath + ": (" + str(response.status_code) + ") " + json.dumps(response.json()) + "\n")
+            # printJsonResponse(imagePath, response)
+            printSimplePrediction(imagePath, response)
         except Exception as e:
             print(e)
             print("No response from classification service")
             return None
 
     return json.dumps(response.json())
+
+def printJsonResponse(imagePath, response):
+    print("Response from classification service for image " + imagePath + ": (" + str(response.status_code) + ") " + json.dumps(response.json()) + "\n")
+
+def printSimplePrediction(imagePath, response):
+    predictions = []
+    for prediction in response.json()['predictions']:
+        predictions.append(prediction['probability'])
+    
+    streetProbability = predictions[0]
+    carProbability = predictions[1]
+
+    certainty = 0
+    if streetProbability >= 0.9:
+        guess = "STREET"
+        certainty = streetProbability
+    elif carProbability >= 0.9:
+        guess = "CAR"
+        certainty = carProbability
+    else: 
+        guess = "VERY SADLY UNKNOWN"
+
+    isGuessRight = ""
+    if ("street" in imagePath.lower() and guess == "STREET") or ("car" in imagePath.lower() and guess == "CAR"):
+        isGuessRight = "THE GUESS IS FUCKING RIGHT MOTHAFOCKA"
+    else:
+        isGuessRight = "WARNING !!!!1 WRONG GUESS"
+
+    print("The code is " + str(f"{certainty*100:.2f}")  + "%% sure that "  + imagePath + " is a f*cking " + guess + " ---------------->" + isGuessRight)
 
 def get_jpg_files(folder_name):
     folder_path = os.path.join(os.path.dirname(__file__), folder_name)
@@ -59,12 +88,13 @@ def main(imagePath, imageProcessingEndpoint):
         print ( "The sample is now sending images for processing and will indefinitely.")
 
         while True:
+            print("\n This is the start of the iteration")
             jpg_files = get_jpg_files(imagePath)
             for image in jpg_files:
                 classification = sendFrameForProcessing(image, imageProcessingEndpoint)
                 if classification:
                     send_to_hub(classification)
-                time.sleep(10)
+                time.sleep(5)
 
     except KeyboardInterrupt:
         print ( "IoT Edge module sample stopped" )
@@ -72,8 +102,10 @@ def main(imagePath, imageProcessingEndpoint):
 if __name__ == '__main__':
     try:
         # Retrieve the image location and image classifying server endpoint from container environment
-        IMAGE_PATH = os.getenv('IMAGE_PATH', "")
-        IMAGE_PROCESSING_ENDPOINT = os.getenv('IMAGE_PROCESSING_ENDPOINT', "")
+        # IMAGE_PATH = os.getenv('IMAGE_PATH', "")
+        # IMAGE_PROCESSING_ENDPOINT = os.getenv('IMAGE_PROCESSING_ENDPOINT', "")
+        IMAGE_PATH = "ImagesToClassify/"
+        IMAGE_PROCESSING_ENDPOINT = "http://samplemodule/image"
     except ValueError as error:
         print ( error )
         sys.exit(1)
